6bf2b3c491eb642ba317074dd17465cb03d1ff10
Alex Nichol <alexnichol@comcast.net>
2011-11-12 11:26:53 -0500

    Initial Commit

---
+++ RSSApi/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RSSApi
+//
+//  Created by Alex Nichol on 11/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
